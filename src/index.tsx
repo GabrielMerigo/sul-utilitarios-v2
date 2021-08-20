8005b67238253f378023dd26046c9f30255249e4
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-20 01:29:10 -0300

    adicionando axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createServer, Model } from 'miragejs';
+
+const transaction = 'transaction';
+const url = '/transactions';
+
+createServer({
+  models: {
+    transactions: Model
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          img: './assets/carro-do-pai.png',
+          title: 'FORD ECOSPORT 2005 XLS',
+          subtitle: '1.6 Ano 2004/2005 com gnv completa, financia, recebemos troca.'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
