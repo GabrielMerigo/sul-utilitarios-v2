13c760a9e71d5e8ec088b50aae9f98d5bfeae592
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-25 01:17:01 -0300

    Section local concluído

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Header } from "../../components/Header";
 import Banner from '../../assets/banner-black.png';
 import { IoIosCar } from 'react-icons/io';
 import { RiInformationLine } from 'react-icons/ri';
-import { Img, HighlightedVehicles, CarList, Description, Local } from './styles'
+import { Img, HighlightedVehicles, CarList, Description, Local, Map } from './styles'
 import ArrowRight from '../../assets/arrow-right.png';
 import ArrowLeft from '../../assets/arrow-left.png';
 import { api } from "../../services/api";
@@ -87,12 +87,16 @@ export function Home() {
         </div>
       </Description>
       <Local>
-        <h2>Onde Estamos?</h2>
-        <div>
+        <div className="line-title">
+          <div></div>
+          <h2>Onde Estamos?</h2>
+        </div>
+
+        <Map>
           <div>
             <iframe src="https://maps.google.com/maps?width=595&height=400&hl=en&q=Avenida%20sertorio,7140%20Porto%20Alegre+(Loja%20-%20Sul%20Ultilit%C3%A1rios)&t=&z=16&ie=UTF8&iwloc=B&output=embed"></iframe>
           </div>
-        </div>
+        </Map>
       </Local>
     </>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -175,7 +175,47 @@ export const Description = styled.div`
 `;
 
 export const Local = styled.div`
-  h2{
-    
+   .line-title{
+    max-width: 900px;
+    margin: 0.4rem auto;
+    position: relative;
+    text-align: center;
+
+    div{
+      margin-top: 1.4rem;
+      position: absolute;
+      border: 1px solid black;
+      background-color: black;
+      width: 100%;
+    }
+
+    h2 {
+      font-size: 2rem;
+      background: white;
+      position: relative;
+      display: inline-block;
+      text-align: center;
+      padding: 0 1rem;
+      text-transform: uppercase;
+      font-style: italic;
+    }
+  }
+`;
+
+
+export const Map = styled.section`
+	border-top: 2px solid #333333;
+	background: #5c5c5c;
+	height: 30rem;
+	width: 100%;
+  display: flex;
+  justify-content: center;
+  
+
+  iframe{
+    border: 3px solid #333333;
+    margin: 1.5rem auto;
+    height: 432px;
+    width: 47.5rem;
   }
 `;
